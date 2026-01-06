af7d0f2114a1b74987d77878c4f87fdbca6f73e6
raethira <rahul.ethiraj@gmail.com>
2026-01-05 17:48:19 -0800

    SquaresOfASortedArray

---
+++ CrashCourse/ArraysAndStrings/SquaresOfASortedArray/code.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        
+        while (left <= right):
+            leftSq = nums[left] * nums[left]
+            rightSq = nums[right] * nums[right]
+            
+            if leftSq < rightSq:
+                result [right - left] = rightSq
+                right -= 1
+            else:
+                result [right - left] = leftSq
+                left += 1
+            
+        return result
+        
+        
+            
+        
